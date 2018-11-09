26eb64b52b03ab0a4695a219e9aee9722e9ef1bd
santos-monica <monicasantosdev@gmail.com>
2018-11-09 16:52:11 -0200

    integracao react e redux (incompleto)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
 import Timeline from './components/Timeline';
-import {createStore, applyMiddleware, combineReducers} from 'redux';
-import { timeline } from "./reducers/timeline";
-import { notificacao } from "./reducers/header";
-import thunkMiddleware from 'redux-thunk';
-
-const reducers = combineReducers({ timeline, notificacao });
-const store = createStore(reducers, applyMiddleware(thunkMiddleware));
 class App extends Component {
   render() {
-    console.log(this.props)
     return (
       <div id="root">
         <div className="main">
-          <Header store={store}/>
-				<Timeline login={this.props.login} store={store} />
+          <Header store={this.context.store} />
+          <Timeline login={this.props.login} store={this.context.store} />
         </div>
       </div>
     );
   }
   
+  
+}
+
+App.contextTypes = {
+  store: React.PropTypes.object.isRequired
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import Logout from './components/Logout';
 import './css/timeline.css';
 import './css/reset.css';
 import './css/login.css';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { timeline } from "./reducers/timeline";
+import { notificacao } from "./reducers/header";
+import { Provider } from 'react-redux';
 
 function verificaAutenticacao(nextState) {
     const match = matchPath('/timeline', {
@@ -18,7 +23,7 @@ function verificaAutenticacao(nextState) {
     if (match !== null) {
         valida = match.isExact
     }
-
+    
     if (valida && localStorage.getItem('auth-token') === null) {
         return <Redirect to={{
             pathname: '/',
@@ -28,16 +33,21 @@ function verificaAutenticacao(nextState) {
     return <App login={nextState.match.params.login} />
 }
 
+const reducers = combineReducers({ timeline, notificacao });
+const store = createStore(reducers, applyMiddleware(thunkMiddleware));
+
 ReactDOM.render((
-    <BrowserRouter>
-        <div>
-            <Switch>
-                <Route exact path="/" component={Login}/>
-                <Route path="/timeline/:login?" render={verificaAutenticacao} />
-                <Route exact path="/logout" component={Logout}/>
-            </Switch>
-        </div>
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/timeline/:login?" render={verificaAutenticacao} />
+                    <Route exact path="/logout" component={Logout}/>
+                </Switch>
+            </div>
+        </BrowserRouter>
+    </Provider>
 
 ), document.getElementById('root'));
 
